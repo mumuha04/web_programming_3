f0e6c7dbbabc072a778fd632b7d2b151017cb142
Mumuha04 <muhammmadnurfadilah28@gmail.com>
2023-11-23 20:42:59 +0700

    Membuat halaman tampilan cetak buku,  Membuat dan mencetak laporan data buku dengan media printer

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -72,7 +72,31 @@
 
             </li>
             </li>
+            <div class="sidebar-heading">
+                Laporan
+            </div>
+            <!-- Nav Item - Dashboard -->
+            <li class="nav-item active">
+                <!-- Nav Item - Dashboard -->
+            <li class="nav-item">
+                <a class="nav-link pb-0" href="<?= base_url('buku/kategori'); ?>">
+                    <i class="fa fa-fw fa book"></i>
+                    <span>Laporan Data Buku</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link pb-0" href="<?= base_url('buku'); ?>">
+                    <i class="fa fa-fw fa book"></i>
+                    <span>Laporan Data Anggota</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link pb-0" href="<?= base_url('user/anggota'); ?>">
+                    <i class="fa fa-fw fa book"></i>
+                    <span>Laporan Data Peminjaman</span></a>
+            </li>
+            </li>
 
+            <!-- Divider -->
+            <hr class="sidebar-divider mt-3">
             <!-- Sidebar Toggler (Sidebar) -->
             <div class="text-center d-none d-md-inline">
                 <button class="rounded-circle border-0" id="sidebarToggle"></button>
